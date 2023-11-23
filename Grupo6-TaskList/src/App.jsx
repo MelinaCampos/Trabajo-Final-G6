82f5920367464e925087f55df96fc689888361f8
MelinaCampos <112991099+MelinaCampos@users.noreply.github.com>
2023-11-22 22:42:09 -0300

    Update App.jsx

--- Grupo6-TaskList/src/App.jsx
+++ Grupo6-TaskList/src/App.jsx
@@ -1,17 +1,55 @@
 import './App.css'
-import { CreadorTareas } from './assets/Componentes/CreadorTareas/CreadorTareas'
-import { ListaTarea } from "./assets/Componentes/CreadorTareas/Tareas/ListaTarea.1";
-import { useState } from 'react';
+import React, { useState } from 'react'
+import TaskList from './components/TaskList/TaskList'
+import TaskForm from './components/TaskForm/TaskForm'
 
 
-function App() {
+const App = () => {
+  const [tareas, setTareas] = useState([]);
+
+  const agregarTarea = (nombreTarea) => {
+    const nuevaTarea = {
+      id: tareas.length + 1,
+      nombre: nombreTarea,
+      completada: false,
+    };
+
+    setTareas((tareasPrevias) => [...tareasPrevias, nuevaTarea]);
+  };
+
+  const handleToggleCompletar = (tareaId) => {
+    setTareas((tareasPrevias) =>
+      tareasPrevias.map((tarea) =>
+        tarea.id === tareaId ? { ...tarea, completada: !tarea.completada } : tarea
+      )
+    );
+  };
+
+  const handleEliminar = (taskId) => {
+    setTareas((tareasPrevias) => tareasPrevias.filter((tarea) => tarea.id !== taskId));
+  };
+
+  const countCompletadas = () => {
+    return tareas.filter((tarea) => tarea.completada).length;
+  };
+
+  const countPendientes = () => {
+    return tareas.length - countCompletadas();
+  };
+
   return (
-    <>
-     <CreadorTareas/>
+    <div>
+      <h1>Lista de Tareas</h1>
+      <h3>Pendientes: {countPendientes()} </h3>
+      <h3>Completadas: {countCompletadas()}</h3>
+      <TaskForm onAgregarTarea={agregarTarea} />
+      <TaskList
+        tareas={tareas}
+        onToggleCompletar={handleToggleCompletar}
+        onEliminar={handleEliminar}
+      />
+    </div>
+  );
+};
 
-    </>
-  )
-}
-
-export default App
-
+export default App;
