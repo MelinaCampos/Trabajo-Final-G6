631e70d9e9d5cfaa53c48e6b970756558184e8eb
MelinaCampos <112991099+MelinaCampos@users.noreply.github.com>
2023-11-23 09:21:32 -0300

    Update TaskItem.jsx

--- Grupo6-TaskList/src/components/TaskItem/TaskItem.jsx
+++ Grupo6-TaskList/src/components/TaskItem/TaskItem.jsx
@@ -3,9 +3,9 @@ import React from 'react'
 
 const TaskItem = ({ nombre, completada, onToggleCompletar, onEliminar }) => {
     return (
-      <div className='boxTarea' style={{ textDecoration: completada ? 'line-through' : 'none' }}>
+      <div className='boxTarea' style={{ textDecoration: completada ? 'line-through' : 'none' }}> //tacha la tarea una vez completada
         <span>{nombre}</span>
-        <input   // agrega un checkbox que cuando cambie ejecute la funcion que permite que se tache una tarea cuando esta completada
+        <input   // agrega un checkbox que cuando cambie ejecute la funcion que permite que cambie el estado de completada de la tarea
           type="checkbox"
           checked={completada}
           onChange={onToggleCompletar}
