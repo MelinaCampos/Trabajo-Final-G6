ba52c3a4c70a5d77da8817c6237d6f4533dac3ae
g0blin1983 <91135018+g0blin1983@users.noreply.github.com>
2023-11-24 09:41:14 -0300

    Orden

---
+++ src/components/TaskList/TaskList.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+import  TaskItem  from '../TaskItem/TaskItem'
+
+const TaskList = ({ tareas, onToggleCompletar, onEliminar }) => {
+    return (
+      //iterando sobre el array tareas para generar el conjunto de elementos TaskItem con el contenido del array
+      <div>
+        {tareas.map((tarea) => (
+          <TaskItem
+            key={tarea.id}
+            nombre={tarea.nombre}
+            completada={tarea.completada}
+            onEliminar={() => onEliminar(tarea.id)}
+            onToggleCompletar={() => onToggleCompletar(tarea.id)}
+          />
+        ))}
+      </div>
+    );
+  };
+  
+  export default TaskList;
