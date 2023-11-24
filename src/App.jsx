6b6279dc70616b1696b6f1075a81b1409be182aa
g0blin1983 <91135018+g0blin1983@users.noreply.github.com>
2023-11-24 10:35:14 -0300

    Corrección!!

--- src/App.jsx
+++ src/App.jsx
@@ -3,27 +3,55 @@ import React, { useEffect, useState } from 'react'
 import TaskList from './components/TaskList/TaskList'
 import TaskForm from './components/TaskForm/TaskForm'
 import { Grid, Typography } from '@mui/material'
+import GitHubIcon from '@mui/icons-material/GitHub'
+import { v4 as uuidv4 } from 'uuid'
 
 const App = () => {
 
+  
   const [tareas, setTareas] = useState(() => {
     //Inicialización de localStorage o un array vacio.
     const storedTareas = JSON.parse(localStorage.getItem('tareas'));
     return storedTareas || [];
   });
 
+
   //Guardar tarea en localStorage cada que actualiza
   useEffect(()=>{
     localStorage.setItem('tareas',JSON.stringify(tareas));
   }, [tareas]);
 
+  const [tareasEliminadas, setTareasEliminadas] = useState(()=>{
+    const storedTareasEliminadas = JSON.parse(localStorage.getItem('tareasEliminadas'));
+    return storedTareasEliminadas || []
+  });
+
+  useEffect(()=>{
+    localStorage.setItem('tareasEliminadas', JSON.stringify(tareasEliminadas));
+  },[tareasEliminadas])
+  
   const agregarTarea = (nombreTarea) => {
+    // Validar si ya existe una tarea con el mismo nombre
+    const tareaExistente = tareas.find(tarea => tarea.nombre === nombreTarea);
+
+    if (tareaExistente) {
+      alert("Ya existe una tarea con el mismo nombre");
+      return;
+    }
+
+    const nuevaTarea = {
+      id: uuidv4(),
+      nombre: nombreTarea,
+      completada: false,
+    };
+  
+  /*const agregarTarea = (nombreTarea) => {
     const nuevaTarea = {
       id: tareas.length + 1,
       nombre: nombreTarea,
       completada: false,
-    };
-
+    };*/
+  
     setTareas((tareasPrevias) => [...tareasPrevias, nuevaTarea]);
   };
 
@@ -36,7 +64,14 @@ const App = () => {
   };
 
   const handleEliminar = (taskId) => {
-    setTareas((tareasPrevias) => tareasPrevias.filter((tarea) => tarea.id !== taskId));
+    //setTareas((tareasPrevias) => tareasPrevias.filter((tarea) => tarea.id !== taskId));
+  
+    const tareaEliminada = tareas.find((tarea => tarea.id === taskId));
+    if (tareaEliminada){
+      setTareasEliminadas((tareasEliminadasPrevias)=>[...tareasEliminadasPrevias, tareaEliminada]);
+      setTareas((tareasPrevias) => tareasPrevias.filter((tarea)=> tarea.id !== taskId))
+    }
+
   };
 
   const countCompletadas = () => {
@@ -45,6 +80,16 @@ const App = () => {
 
   const countPendientes = () => {
     return tareas.length - countCompletadas();
+  };
+  
+  
+  const mostrarTareasEliminadas = () => {
+    if (tareasEliminadas.length === 0) {
+      alert("No hay tareas eliminadas");
+      return;
+    }
+
+    alert("Tareas eliminadas:\n" + tareasEliminadas.map(tarea => tarea.nombre).join('\n'));
   };
 
   return (
@@ -64,6 +109,8 @@ const App = () => {
       </Grid>
       <Grid item xs={12}>
         <TaskForm onAgregarTarea={agregarTarea} />
+        <Typography variant="h6">Tareas Eliminadas</Typography>
+        <button onClick={mostrarTareasEliminadas}>Mostrar Tareas Eliminadas</button>
       </Grid>
       <Grid item xs={12}>
         <TaskList
@@ -72,6 +119,11 @@ const App = () => {
           onEliminar={handleEliminar}
         />
       </Grid>
+        <footer><h6>
+          <GitHubIcon/><a href='https://github.com/MelinaCampos' target='_blank'>Meli</a></h6>
+          <h6><GitHubIcon/><a href='https://github.com/g0blin1983' target='_blank'>Gabriel</a></h6>
+          <h6><GitHubIcon/><a href='https://github.com/LautaroScherer' target='_blank'>Sebastian</a></h6>
+        </footer>
     </Grid>
   );
 };
